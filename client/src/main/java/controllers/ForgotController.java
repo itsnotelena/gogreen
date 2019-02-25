4073b20d9108f4d94bbaa24b9b4e32eeaebf0d46
Victor Roest <V.C.Roest@student.tudelft.nl>
2019-02-25 10:51:50 +0100

    Fixed checkstyle warnings

--- client/src/main/java/controllers/ForgotController.java
+++ client/src/main/java/controllers/ForgotController.java
@@ -1,5 +1,7 @@
 package controllers;
 
+import static com.sun.javafx.scene.control.skin.Utils.getResource;
+
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -17,8 +19,6 @@ import org.springframework.stereotype.Component;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
-
-import static com.sun.javafx.scene.control.skin.Utils.getResource;
 
 @Component
 @ComponentScan({"client"})
