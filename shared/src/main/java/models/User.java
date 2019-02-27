68a7215b1bb55c70b8a79dfff2925b5f120e2ff2
Victor Roest <V.C.Roest@student.tudelft.nl>
2019-02-27 17:04:31 +0100

    Checkstyle fix

--- shared/src/main/java/models/User.java
+++ shared/src/main/java/models/User.java
@@ -4,8 +4,14 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 
-import javax.persistence.*;
 import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 
 @Entity
 @Table(name = "user")
