9110f78ff8eea02f4a5275b8b27980b87e95ff54
Vykintas <v.civas-1@student.tudelft.nl>
2019-03-01 14:02:40 +0100

    Fixed checkstyle

--- shared/src/main/java/models/Log.java
+++ shared/src/main/java/models/Log.java
@@ -1,14 +1,19 @@
 package models;
 
-
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
 import java.io.Serializable;
 import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
 @Entity
 @Table(name = "log")
@@ -27,7 +32,7 @@ public class Log implements Serializable {
     //add action class here
 
     @Column
-    Date date;
+    private Date date;
 
 
 
--- shared/src/main/java/models/User.java
+++ shared/src/main/java/models/User.java
@@ -8,7 +8,13 @@ import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
 
 
 @Entity
