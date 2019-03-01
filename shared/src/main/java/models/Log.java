49039142444e2cd2801acd12a7030ced2ef5d85b
Vykintas <v.civas-1@student.tudelft.nl>
2019-03-01 14:08:08 +0100

    Fixed Checkstyle

--- shared/src/main/java/models/Log.java
+++ shared/src/main/java/models/Log.java
@@ -5,15 +5,14 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.io.Serializable;
-import java.util.Date;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import java.io.Serializable;
+import java.util.Date;
 
 @Entity
 @Table(name = "log")
@@ -33,7 +32,5 @@ public class Log implements Serializable {
 
     @Column
     private Date date;
-
-
 
 }
