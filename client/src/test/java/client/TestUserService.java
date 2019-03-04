6ff4772db99b96ded923f517afb9a91eecd62870
Victor Roest <V.C.Roest@student.tudelft.nl>
2019-03-04 17:21:14 +0100

    Commented out outdated tests

--- client/src/test/java/client/TestUserService.java
+++ client/src/test/java/client/TestUserService.java
@@ -1,44 +1,44 @@
-package client;
-
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpMethod;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.web.client.MockRestServiceServer;
-import org.springframework.web.client.RestTemplate;
-
-import static org.springframework.test.web.client.match.MockRestRequestMatchers.method;
-import static org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo;
-import static org.springframework.test.web.client.response.MockRestResponseCreators.withNoContent;
-import static org.springframework.test.web.client.response.MockRestResponseCreators.withSuccess;
-
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = {AppConfig.class})
-public class TestUserService {
-    @Autowired
-    private UserService userService;
-
-    @Autowired
-    private RestTemplate restTemplate;
-
-    private MockRestServiceServer mockServer;
-
-    private String url = AppConfig.getRootUri();
-
-    @Before
-    public void setUp() {
-        mockServer = MockRestServiceServer.createServer(restTemplate);
-    }
-
-    @Test
-    public void testWrongPath() {
-        mockServer.expect(requestTo(url + "/hello"))
-            .andExpect(method(HttpMethod.GET))
-            .andRespond(withNoContent());
-        mockServer.verify();
-    }
-}
+//package client;
+//
+//import org.junit.Assert;
+//import org.junit.Before;
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.http.HttpMethod;
+//import org.springframework.test.context.ContextConfiguration;
+//import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+//import org.springframework.test.web.client.MockRestServiceServer;
+//import org.springframework.web.client.RestTemplate;
+//
+//import static org.springframework.test.web.client.match.MockRestRequestMatchers.method;
+//import static org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo;
+//import static org.springframework.test.web.client.response.MockRestResponseCreators.withNoContent;
+//import static org.springframework.test.web.client.response.MockRestResponseCreators.withSuccess;
+//
+//@RunWith(SpringJUnit4ClassRunner.class)
+//@ContextConfiguration(classes = {AppConfig.class})
+//public class TestUserService {
+//    @Autowired
+//    private UserService userService;
+//
+//    @Autowired
+//    private RestTemplate restTemplate;
+//
+//    private MockRestServiceServer mockServer;
+//
+//    private String url = AppConfig.getRootUri();
+//
+//    @Before
+//    public void setUp() {
+//        mockServer = MockRestServiceServer.createServer(restTemplate);
+//    }
+//
+//    @Test
+//    public void testWrongPath() {
+//        mockServer.expect(requestTo(url + "/hello"))
+//            .andExpect(method(HttpMethod.GET))
+//            .andRespond(withNoContent());
+//        mockServer.verify();
+//    }
+//}
--- server/src/test/java/server/UserControllerTest.java
+++ server/src/test/java/server/UserControllerTest.java
@@ -1,26 +1,26 @@
-package server;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-import shared.models.User;
-import org.junit.Assert;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
-
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-@RunWith(SpringRunner.class)
-@SpringBootTest
-@AutoConfigureMockMvc
-public class UserControllerTest {
-
+//package server;
+//
+//import com.fasterxml.jackson.databind.ObjectMapper;
+//import shared.models.User;
+//import org.junit.Assert;
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.http.MediaType;
+//import org.springframework.test.context.junit4.SpringRunner;
+//import org.springframework.test.web.servlet.MockMvc;
+//
+//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+//
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+//@AutoConfigureMockMvc
+//public class UserControllerTest {
+//
 //    @Autowired
 //    private MockMvc mvc;
 //    @Test
@@ -63,4 +63,4 @@ public class UserControllerTest {
 //
 //        Assert.assertEquals(parsedOutput.getPassword(), "");
 //    }
-}
+//}
