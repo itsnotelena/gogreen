73242d92e4636d6fb276d425c355939c206a020b
agata <a.n.kordyl@student.tudelft.nl>
2019-03-04 13:17:00 +0100

    Version with MAIN working + all the files BUT side menu not implemented + no icon pictures

--- client/src/main/java/controllers/MenuController.java
+++ client/src/main/java/controllers/MenuController.java
@@ -1,24 +1,44 @@
 package controllers;
 
+import com.jfoenix.controls.JFXDrawer;
+import com.jfoenix.controls.JFXHamburger;
+import com.jfoenix.transitions.hamburger.HamburgerSlideCloseTransition;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.geometry.Rectangle2D;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.TabPane;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
-import java.util.Objects;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 import static com.sun.javafx.scene.control.skin.Utils.getResource;
 
 @Component
-public class MenuController {
+public class MenuController extends MainController  {
     @FXML
     private TabPane menupane;
+
+    @FXML
+    private AnchorPane anchor;
+
+    @FXML
+    private Pane myPane;
+
+    @FXML
+    private JFXDrawer drawer;
+
+    @FXML
+    private JFXHamburger hamburger;
 
     /**
      * Logs the user out and shows them the login screen.
@@ -29,8 +49,42 @@ public class MenuController {
         Stage stage = (Stage) menupane.getScene().getWindow();
         Parent root = FXMLLoader.load(getResource("/window1.fxml"));
         stage.setTitle("Go Green");
-        Rectangle2D screenSize = Screen.getPrimary().getVisualBounds();
         stage.setScene(new Scene(root, 600, 500));
         /* stage.show(); */
     }
-}
+
+//    @FXML
+//    private void sideBar() throws IOException {
+//        myPane = FXMLLoader.load(getClass().getResource( "/toolbar.fxml" ) );
+//
+//        drawer.setSidePane(myPane);
+//
+//        HamburgerSlideCloseTransition task = new HamburgerSlideCloseTransition(hamburger);
+//        task.setRate( -1 );
+//
+//        hamburger.addEventHandler( MouseEvent.MOUSE_CLICKED, (event) -> {
+//                task.setRate(task.getRate() * -1 );
+//                task.play();
+//
+//              if (drawer.isOpened()) {
+//                   drawer.close();
+//                } else {
+//                    drawer.open();
+//        } });
+//              drawer.setOnDrawerOpened(event -> {
+//                  AnchorPane.setRightAnchor(drawer, 0.0);
+//                  AnchorPane.setLeftAnchor(drawer, 0.0);
+//                  AnchorPane.setTopAnchor(drawer, 0.0);
+//                  AnchorPane.setBottomAnchor(drawer, 0.0);
+//              });
+//
+//              drawer.setOnDrawerClosed(event ->
+//             {
+//                  AnchorPane.clearConstraints(drawer);
+//                  AnchorPane.setLeftAnchor(drawer, -150.0);
+//                  AnchorPane.setTopAnchor(drawer, 0.0);
+//                  AnchorPane.setBottomAnchor(drawer, 0.0);
+//             });
+//    }
+
+    }
