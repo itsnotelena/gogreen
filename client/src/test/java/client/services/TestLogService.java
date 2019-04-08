cfbb440f15678de51fada72c15a2532a3c5e5201
Alin Dondera <A.Dondera@student.tudelft.nl>
2019-04-08 20:21:18 +0200

    covered a few more branches

--- client/src/test/java/client/services/TestLogService.java
+++ client/src/test/java/client/services/TestLogService.java
@@ -2,12 +2,15 @@ package client.services;
 
 import client.AppConfig;
 import client.services.UserService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.client.MockRestServiceServer;
@@ -19,8 +22,7 @@ import shared.models.User;
 
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.method;
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo;
-import static org.springframework.test.web.client.response.MockRestResponseCreators.withNoContent;
-import static org.springframework.test.web.client.response.MockRestResponseCreators.withSuccess;
+import static org.springframework.test.web.client.response.MockRestResponseCreators.*;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = {AppConfig.class})
@@ -51,5 +53,22 @@ public class TestLogService {
         logService.createLog(log);
 
         mockServer.verify();
+    }
+
+    @Test
+    public void createLogWrongUserTest() throws Exception {
+        ObjectMapper mapper = new ObjectMapper();
+        Log log = new Log();
+        log.setAction(Action.VEGETARIAN);
+        mockServer.expect(requestTo(url + UserEndpoints.LOGS))
+                .andExpect(method(HttpMethod.POST))
+                .andRespond(withStatus(HttpStatus.OK)
+                        .contentType(MediaType.APPLICATION_JSON).body(mapper.writeValueAsString(log)));
+
+        boolean toCompare = logService.createLog(log);
+
+        mockServer.verify();
+
+        Assert.assertTrue(toCompare);
     }
 }
